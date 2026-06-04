8a335e28ce6b746bfbfd6561e73d49da959bcae1
axmadboy033-create <axmadboy033@gmail.com>
2026-06-04 07:39:28 +0500

    commit

---
+++ src/components/contacts/Contacts.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Contacts = () => {
+  return (
+    <div>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, ipsum.</p>
+    </div>
+  )
+}
+
+export default Contacts
