da80b449f5bce35866815ca8560753b400e54b88
axmadboy033-create <axmadboy033@gmail.com>
2026-06-04 08:03:03 +0500

    Contact komponenti

---
+++ src/components/contacts/Contact.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Distinctio soluta eligendi quia.</p>
+
+    </div>
+  )
+}
+
+export default Contact
